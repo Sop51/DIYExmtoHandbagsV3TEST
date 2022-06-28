fa82a7feb2d626a5025d9c731997b01454e0715c
marcotts <marcotts@umich.edu>
2022-06-28 16:20:43 +0200

    Initial Commit

---
+++ DIYExmtoHandbagsV3TEST/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DIYExmtoHandbagsV3TEST
+//
+//  Created by Sophie Marcotte on 6/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DIYExmtoHandbagsV3TEST/DIYExmtoHandbagsV3TESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  DIYExmtoHandbagsV3TESTApp.swift
+//  DIYExmtoHandbagsV3TEST
+//
+//  Created by Sophie Marcotte on 6/28/22.
+//
+
+import SwiftUI
+
+@main
+struct DIYExmtoHandbagsV3TESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
